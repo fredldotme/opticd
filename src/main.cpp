070a252544e47800e47fe93e05ec45b4b8d4bde4
Alfred Neumayer <dev.beidl@gmail.com>
2021-12-27 21:57:05 +0100

    treewide: Remove the capabilities dance
    No need to drop and regain capabilities anymore, so drop it
    and the need for a suid binary on the filesystem.

--- src/main.cpp
+++ src/main.cpp
@@ -53,135 +53,14 @@ static void sig_handler(int sig_num)
     exit(0);
 }
 
-static inline void setCapsOrDie()
-{
-    static const std::array<cap_value_t, 4> necessaryCaps {
-        CAP_CHOWN, CAP_SETUID, CAP_SETGID, CAP_SYS_PTRACE
-    };
-    cap_t caps = cap_init();
-
-    // Auto-free caps object when done
-    auto capfree = [](cap_t* c){ cap_free(c); };
-    std::unique_ptr<cap_t, decltype(capfree)> capcleaner(&caps, capfree);
-
-    if (cap_set_flag(caps, CAP_PERMITTED, necessaryCaps.size(), necessaryCaps.data(), CAP_SET) != 0) {
-        qFatal("Failed to set permitted caps: %s", strerror(errno));
-        exit(1);
-        return;
-    }
-    if (cap_set_flag(caps, CAP_EFFECTIVE, necessaryCaps.size(), necessaryCaps.data(), CAP_SET) != 0) {
-        qFatal("Failed to set effective caps: %s", strerror(errno));
-        exit(1);
-        return;
-    }
-    if (cap_set_proc(caps) != 0) {
-        qFatal("Failed to set process caps: %s", strerror(errno));
-        exit(1);
-        return;
-    }
-}
-
-static inline void dropPrivsOrDie()
-{
-    const int groupId = getuid();
-    const int userId = getuid();
-    static const std::vector<std::string> necessaryGroups = {
-        "video", "android_graphics"
-    };
-
-    qInfo("Running as uid: %d", userId);
-    qInfo("Euid: %d", geteuid());
-
-    if (geteuid() == 0) {
-        int ngroups = sysconf(_SC_NGROUPS_MAX) + 1;
-        gid_t* groups = (gid_t*) malloc(ngroups * sizeof(gid_t));
-
-        std::vector<gid_t> newGroups;
-
-        struct passwd* pw = getpwuid(userId);
-        if (!pw) {
-            qFatal("Failed to get passwd entry: %s", strerror(errno));
-            exit(1);
-            return;
-        }
-
-        if (getgrouplist(pw->pw_name, pw->pw_gid, groups, &ngroups) == -1) {
-            qFatal("getgrouplist() returned -1; ngroups = %d", ngroups);
-            exit(1);
-            return;
-        }
-
-        for (int i = 0; i < ngroups; i++) {
-            gid_t& group = groups[i];
-            struct group* groupStruct = getgrgid(group);
-            if (!groupStruct) {
-                qFatal("Failed to get group name for gid %d", group);
-                exit(1);
-                return;
-            }
-
-            if (std::find(necessaryGroups.begin(), necessaryGroups.end(),
-                          std::string(groupStruct->gr_name)) != necessaryGroups.end()) {
-                newGroups.push_back(group);
-            }
-        }
-
-        if (newGroups.size() != necessaryGroups.size()) {
-            qFatal("Not enough sauce.");
-            exit(1);
-            return;
-        }
-
-        if (setgroups(newGroups.size(), newGroups.data()) != 0) {
-            qFatal("Failed to set new supplementary groups, bailing...");
-            exit(1);
-            return;
-        }
-
-        free(groups);
-
-        if (setgid(groupId) != 0) {
-            qFatal("Failed to drop group privileges: %s", strerror(errno));
-            exit(1);
-            return;
-        }
-        if (setuid(userId) != 0) {
-            qFatal("Failed to drop user privileges: %s", strerror(errno));
-            exit(1);
-            return;
-        }
-    }
-
-    if (setuid(0) != -1) {
-        qFatal("Managed to regain root privileges, bailing...");
-        exit(1);
-        return;
-    }
-
-    // Lomiri needs to read the environment or arguments of this process...
-    // ... fine! We still have CAP_CHOWN.
-    if (prctl(PR_SET_DUMPABLE, 1) < 0) {
-        qFatal("Failed to set dumpable flag: %s", strerror(errno));
-        exit(1);
-        return;
-    }
-}
-
 int main(int argc, char *argv[])
 {
-    // Before doing ANYTHING, keep PTRACE capability and drop privileges
-    setCapsOrDie();
-    dropPrivsOrDie();
-
     // Get to the chopper
     chdir("/");
 
     EGLDisplay display;
     EGLContext context;
     EGLSurface surface;
-
-    // Paranoid power regain failed anyway, it's safe to allow suid
-    QCoreApplication::setSetuidAllowed(true);
 
     QCoreApplication a(argc, argv);
 
